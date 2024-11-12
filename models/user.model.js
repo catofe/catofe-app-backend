d8bd91708eb80e93fc64d96e1f65ba01557b050a
Nutty-Nuts <vegagerard1130@gmail.com>
2024-11-12 10:30:00 +0800

    initial implementation of user schema and model

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,23 @@
 import mongoose from "mongoose";
 
-const UserSchema = mongoose.Schema({});
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    contact_no: {
+        type: String,
+        required: false,
+    },
+});
 
 const User = mongoose.model("User", UserSchema);
 
