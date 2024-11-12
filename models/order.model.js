3626a3efc0fbd80d32adeedb98219e61d9ccea3b
Nutty-Nuts <vegagerard1130@gmail.com>
2024-11-12 10:55:56 +0800

    initial implementation of order schema and model

--- models/order.model.js
+++ models/order.model.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
+import { ProductInstanceSchema } from "./product_instance.model";
 
-const OrderSchema = mongoose.Schema({});
+const OrderSchema = mongoose.Schema({
+    prodcutInstances: [ProductInstanceSchema],
+    total: {
+        type: Number,
+        required: true,
+    },
+});
 
 const Order = mongoose.model("Order", OrderSchema);
 
-export default Order;
+export { OrderSchema, Order };
